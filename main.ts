5eb9b01aaf57ddfe55a0a9b356c71be347ba16da
Fuzzy <47428103+FuzzyMakers@users.noreply.github.com>
2019-11-06 13:16:00 -0300

    update

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 namespace Fuzzy_sensor {
     let flag = true;
 
-    //% blockId=mbit_ultrasonic block="Distancia do sensor ultrasonico(cm)"
+    //% blockId=mbit_ultrasonic block="Distancia do sensor ultrasonico (cm)"
     //% weight=98
     //% blockGap=10
     export function Ultrasonic_Car(): number {
