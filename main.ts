08df851d52177b81c3010d5d0f485074e7a50959
Fuzzy <47428103+FuzzyMakers@users.noreply.github.com>
2019-11-06 12:50:55 -0300

    updates

--- main.ts
+++ main.ts
@@ -13,9 +13,9 @@ namespace Fuzzy_sensor {
             pins.digitalWritePin(DigitalPin.P14, 0);
             control.waitMicros(2);
             pins.digitalWritePin(DigitalPin.P14, 1);
-            control.waitMicros(15);
+            control.waitMicros(10);
             pins.digitalWritePin(DigitalPin.P14, 0);
-            let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
-        return  Math.floor(d / 40);
+            let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 20000);
+        return  Math.floor(d/28/2);
     }
 }
