50cbd220aa15e9cb4bdddd613795e8922ce2418f
Fuzzy <47428103+FuzzyMakers@users.noreply.github.com>
2019-11-07 17:37:21 -0300

    Acrescentando o uso da tela OLED com a biblioteca Muselab

--- main.ts
+++ main.ts
@@ -2,8 +2,6 @@
 namespace Fuzzy_sensor {
 
 
-
-
     export enum DHT11Type {
         //% block="temperature(℃)" enumval=0
         DHT11_temperature_C,
@@ -32,62 +30,86 @@ namespace Fuzzy_sensor {
     }
 
 
-/**
-     * get dht11 temperature and humidity Value
-     * @param dht11pin describe parameter here, eg: DigitalPin.P15     */
-//% blockId="readdht11" block="Valor do sensor de temeperatura e umidade %dht11type| no pino %dht11pin"
-//% weight=98
-//% blockGap=10
+    /**
+         * get dht11 temperature and humidity Value
+         * @param dht11pin describe parameter here, eg: DigitalPin.P15     */
+    //% blockId="readdht11" block="Valor do sensor de temeperatura e umidade %dht11type| no pino %dht11pin"
+    //% weight=98
+    //% blockGap=10
 
 
-export function dht11value(dht11type: DHT11Type, dht11pin: DigitalPin): number {
+    export function dht11value(dht11type: DHT11Type, dht11pin: DigitalPin): number {
 
-    pins.digitalWritePin(dht11pin, 0)
-    basic.pause(18)
-    let i = pins.digitalReadPin(dht11pin)
-    pins.setPull(dht11pin, PinPullMode.PullUp);
-    switch (dht11type) {
-        case 0:
-            let dhtvalue1 = 0;
-            let dhtcounter1 = 0;
-            while (pins.digitalReadPin(dht11pin) == 1);
-            while (pins.digitalReadPin(dht11pin) == 0);
-            while (pins.digitalReadPin(dht11pin) == 1);
-            for (let i = 0; i <= 32 - 1; i++) {
+        pins.digitalWritePin(dht11pin, 0)
+        basic.pause(18)
+        let i = pins.digitalReadPin(dht11pin)
+        pins.setPull(dht11pin, PinPullMode.PullUp);
+        switch (dht11type) {
+            case 0:
+                let dhtvalue1 = 0;
+                let dhtcounter1 = 0;
+                while (pins.digitalReadPin(dht11pin) == 1);
                 while (pins.digitalReadPin(dht11pin) == 0);
-                dhtcounter1 = 0
-                while (pins.digitalReadPin(dht11pin) == 1) {
-                    dhtcounter1 += 1;
-                }
-                if (i > 15) {
-                    if (dhtcounter1 > 2) {
-                        dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+                while (pins.digitalReadPin(dht11pin) == 1);
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter1 = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter1 += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter1 > 2) {
+                            dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+                        }
                     }
                 }
-            }
-            return ((dhtvalue1 & 0x0000ff00) >> 8);
-            break;
-        case 1:
-            while (pins.digitalReadPin(dht11pin) == 1);
-            while (pins.digitalReadPin(dht11pin) == 0);
-            while (pins.digitalReadPin(dht11pin) == 1);
-
-            let value = 0;
-            let counter = 0;
-
-            for (let i = 0; i <= 8 - 1; i++) {
+                return ((dhtvalue1 & 0x0000ff00) >> 8);
+                break;
+            case 1:
+                while (pins.digitalReadPin(dht11pin) == 1);
                 while (pins.digitalReadPin(dht11pin) == 0);
-                counter = 0
-                while (pins.digitalReadPin(dht11pin) == 1) {
-                    counter += 1;
+                while (pins.digitalReadPin(dht11pin) == 1);
+
+                let value = 0;
+                let counter = 0;
+
+                for (let i = 0; i <= 8 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    counter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        counter += 1;
+                    }
+                    if (counter > 3) {
+                        value = value + (1 << (7 - i));
+                    }
                 }
-                if (counter > 3) {
-                    value = value + (1 << (7 - i));
-                }
-            }
-            return value;
-        default:
-            return 0;
+                return value;
+            default:
+                return 0;
+        }
     }
-}
+    //% blockId="OLEDfuzzy" block="Inicia OLED com largura %larg| e altura %alt "
+    //% weight=87
+    export function IniciaOLED(alt: number, larg: number): void {
+        MuseOLED.init(alt, larg)
+    }
+
+    //% blockId="MostrarString" block="Mostrar string %String "
+    //% weight=87
+    export function MostrarString(String: string): void {
+        MuseOLED.showString(String)
+    }
+    //% blockId="Mostrarnumero" block=" Mostrar numero %Numero "
+    //% weight=80
+    export function Mostrarnumero(Numero: number): void {
+        MuseOLED.showNumber(Numero)
+    }
+    //% blockId="LimparTela" block=" Limpar a tela "
+    //% weight=80
+    export function LimpaTela(): void {
+        MuseOLED.clear()
+    }
+
+
+
 }
